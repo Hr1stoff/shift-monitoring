3ee47b6ea925aa1b5db1d721e9be0840e40f2c31
Hr1stoff <igorvajtenko7@gmail.com>
2025-09-15 18:47:32 +0800

    откл сетификат

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
+import { defineConfig } from 'vite'
+import fs from 'fs'
+import path from 'path'
+
 export default defineConfig({
   server: {
-    host: 'localhost',
-    https: true
+    host: '0.0.0.0', // чтобы было доступно с других устройств
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/cert.pem')),
+    }
   }
 })
